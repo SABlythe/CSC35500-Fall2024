3a80a1a8d795749eb0a19126fdefa53481aed7ac
Stephen Blythe <sblythe@lindenwood.edu>
2024-09-24 12:46:59 -0500

    Added execvp with pipe example

---
+++ processes/pipeExample.cpp
@@ -0,0 +1,63 @@
+#include <cstdio>
+#include <cstring>   // for strlen
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+
+int main()
+{
+  int pipefds[2];
+  int status = pipe(pipefds);
+
+  if (status != 0)
+    {
+      cerr << "pipe() call failed!" << endl;
+      return 1;
+    }
+  
+  int pid= fork();
+
+  if (pid!=0) // parent
+    {
+      close (pipefds[1]); 
+      dup2 ( pipefds[0], fileno(stdin) );
+      
+      cout << "do wc  (parent)" << endl;
+      char *args[2];
+
+      int myLength;// = strlen("wc");
+      string wc = "wc";
+      myLength = wc.length();
+      args[0] = new char [myLength+1];
+      strcpy(args[0], wc.c_str() );
+      args[1] = nullptr;
+      execvp("wc", args);
+      
+    }
+  else //, so in child
+    {
+
+      char *args[3];
+
+      close (pipefds[0]); 
+      dup2 ( pipefds[1], fileno(stdout) );
+      
+      int myLength;// = strlen("wc");
+      string str = "ls";
+      myLength = str.length();
+      args[0] = new char [myLength+1];
+      strcpy(args[0], str.c_str() );
+
+      str = "-l";
+      myLength = str.length();
+      args[1] = new char [myLength+1];
+      strcpy(args[1], str.c_str() );
+      
+      args[2] = nullptr;
+      execvp(args[0], args);
+
+      cout << "do  ls -l   (child)" << endl;
+    }
+  
+  return 0;
+}
